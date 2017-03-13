05988afc9e8bf4ee6f04ee84c94002d17bdcde96
Tristan de Boer <info@tristandeboer.nl>
2017-03-13 18:46:29 +0100

    Added documentation, removed enter.

--- src/main/java/nl/springbank/Main.java
+++ src/main/java/nl/springbank/Main.java
@@ -12,6 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
 
+    /**
+     * Main class. Starts the application.
+     * @param args Arguments passed to Spring.
+     */
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
