482bfe718ab2e84ce1849cab2d80db064bf51e53
Tristan de Boer <info@tristandeboer.nl>
2017-03-15 21:11:04 +0100

    Changed comment

--- src/main/java/nl/springbank/controllers/BankAccountController.java
+++ src/main/java/nl/springbank/controllers/BankAccountController.java
@@ -1,9 +1,13 @@
 package nl.springbank.controllers;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import nl.springbank.bean.BankAccountBean;
+import nl.springbank.bean.UserBean;
 import nl.springbank.dao.BankAccountDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,4 +24,16 @@ public class BankAccountController {
      */
     @Autowired
     private BankAccountDao bankAccountDao;
+
+    @ApiOperation(value = "Get BankAccount",
+            notes = "Returns a BankAccount given a bankAccountId")
+    @ResponseBody
+    @RequestMapping("")
+    public BankAccountBean getUser(Long bankAccountId){
+        try {
+            return bankAccountDao.findOne(bankAccountId);
+        } catch (Exception e){
+            return null;
+        }
+    }
 }
