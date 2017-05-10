963d086bf68c7cb8d2952f77171d08616ad1bf10
Tristan de Boer <info@tristandeboer.nl>
2017-05-10 20:58:07 +0200

    Added test for JsonRpc Error handling

--- src/main/java/nl/springbank/controllers/TestController.java
+++ src/main/java/nl/springbank/controllers/TestController.java
@@ -1,8 +1,11 @@
 package nl.springbank.controllers;
 
+import com.googlecode.jsonrpc4j.JsonRpcError;
+import com.googlecode.jsonrpc4j.JsonRpcErrors;
 import com.googlecode.jsonrpc4j.JsonRpcParam;
 import com.googlecode.jsonrpc4j.JsonRpcService;
 import com.googlecode.jsonrpc4j.spring.AutoJsonRpcServiceImpl;
+import nl.springbank.exceptions.jsonrpc.OtherError;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,7 +17,27 @@ import org.springframework.stereotype.Service;
 @JsonRpcService("/test")
 @AutoJsonRpcServiceImpl
 public class TestController {
+    /**
+     * Multiplies two values.
+     * @param a The first value
+     * @param b The second value
+     * @return a * b
+     */
     public int multiply(@JsonRpcParam(value = "a") int a, @JsonRpcParam(value = "b") int b){
         return a * b;
+    }
+
+    /**
+     * Throws an error.
+     * @param a Some value
+     * @param b Some value
+     * @return
+     * @throws OtherError
+     */
+    @JsonRpcErrors({
+            @JsonRpcError(exception = OtherError.class, code = 500, message = "An unexpected error occured, see error details.")
+    })
+    public int throwsError(@JsonRpcParam(value = "a") int a, @JsonRpcParam(value = "b") int b) throws OtherError {
+        throw new OtherError();
     }
 }
---
+++ src/main/java/nl/springbank/exceptions/jsonrpc/OtherError.java
@@ -0,0 +1,10 @@
+package nl.springbank.exceptions.jsonrpc;
+
+/**
+ * Description
+ *
+ * @author Tristan de Boer.
+ */
+public class OtherError extends Throwable {
+
+}
